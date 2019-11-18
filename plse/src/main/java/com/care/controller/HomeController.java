ba4633f18cc45ec1d8ce422092f74692c6ff080e
berry <berry@DESKTOP-ODD1CT1>
2019-11-18 19:48:53 +0900

    1

--- plse/src/main/java/com/care/controller/HomeController.java
+++ plse/src/main/java/com/care/controller/HomeController.java
@@ -17,6 +17,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	
+	
+	
+	
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -33,7 +38,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "member/main";
 	}
 	
 }
